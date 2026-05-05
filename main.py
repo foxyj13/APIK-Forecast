97a54400b18551bbfdb96003db605797e42fb853
Yuliya Martynova <foxyj13@gmail.com>
2026-05-05 15:15:36 +0700

    Add read station lon-lat, parameter min-max

--- main.py
+++ main.py
@@ -20,11 +20,13 @@ def init_logging(config):
         os.mkdir(log_folder)
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s %(levelname)-7s %(module)s.%(funcName)s - %(message)s",        
+        format="%(asctime)s %(levelname)-7s %(module)s.%(funcName)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
             TimedRotatingFileHandler(
-                filename=os.path.join(log_folder, "meteo.log"), when="midnight", encoding="utf-8"
+                filename=os.path.join(log_folder, "meteo.log"),
+                when="midnight",
+                encoding="utf-8",
             ),
             logging.StreamHandler(sys.stdout),
         ],
@@ -46,6 +48,8 @@ def read_stations(stations_file: str) -> list:
                 "full_name": ws_parameters[f"C{par_row_id}"].value,
                 "short_name": ws_parameters[f"A{par_row_id}"].value,
                 "no_value": ws_parameters[f"E{par_row_id}"].value,
+                "min_value": ws_parameters[f"F{par_row_id}"].value,
+                "max_value": ws_parameters[f"G{par_row_id}"].value,
             }
 
     stations = []
@@ -57,13 +61,25 @@ def read_stations(stations_file: str) -> list:
         par_codes = str(value).split(";")
         for par_code in par_codes:
             par_code = par_code.strip()
-            parameters[all_parameters[par_code]["name"]] = deepcopy(
-                all_parameters[par_code]
-            )
+            if par_code in all_parameters:
+                parameters[all_parameters[par_code]["name"]] = deepcopy(
+                    all_parameters[par_code]
+                )
+            else:  #'Неизвестный режим: "%s".', mode
+                st_name = ws_stations[f"A{st_row_id}"].value
+                st_code = str(ws_stations[f"B{st_row_id}"].value)
+                logging.warning(
+                    "Внимание! Для станции %s (%s) отсутствует параметр в кодом %s. Пропускаю этот параметр.",
+                    st_name,
+                    st_code,
+                    par_code,
+                )
         stations.append(
             {
                 "full_name": ws_stations[f"A{st_row_id}"].value,
                 "code": str(ws_stations[f"B{st_row_id}"].value),
+                "lat": ws_stations[f"E{st_row_id}"].value,
+                "lon": ws_stations[f"F{st_row_id}"].value,
                 "parameters": parameters,
             }
         )
@@ -156,7 +172,9 @@ def main():
         logging.info("Строим статичные графики")
         plotter = Plotter(dict(main_config))
         if db_reader.connect():
-            export_enabled = main_config.getboolean("export-enable") #main_config["export-enable"]
+            export_enabled = main_config.getboolean(
+                "export-enable"
+            )  # main_config["export-enable"]
             for station in stations:
                 station = db_reader.get_station_data(
                     station=station, time_depth=time_depth
