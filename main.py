4b47b9922c0bf619a047a0ea964ac0302189e4f1
Yuliya Martynova <foxyj13@gmail.com>
2026-05-18 18:18:51 +0700

    Add plot global forecasts

--- main.py
+++ main.py
@@ -262,16 +262,18 @@ def main():
                     "Строим статичные графики: наблюдения + глобальный (сырой) прогноз"
                 )
 
-                plotter = Plotter(dict(main_config), glob_forecast=True)
+                plotter = Plotter(
+                    dict(main_config), glob_forecast=True, local_forecast=False
+                )
 
                 if stations:
                     for station in stations:
                         plotter.make_table_forecast(station=station)
-                        # plotter.make_plots_forecast(
-                        #     station=station,
-                        #     time_depth=time_depth,
-                        #     time_forecast=time_forecast,
-                        # )
+                        plotter.make_plots_forecast(
+                            station=station,
+                            time_depth=time_depth,
+                            time_forecast=time_forecast,
+                        )
 
                         # # Экспорт в CSV (если включен в congig.ini)
                         # #   Проверка на включенность опции внутри самой функции export_to_csv
--- plotter.py
+++ plotter.py
@@ -127,6 +127,8 @@ class Plotter:
         )
         plt.close(fig)
 
+        plt.ion()
+
     def make_table_forecast(self, station: dict):
         logging.info(
             "Строю табличку текущих наблюдений для станции %s", station["code"]
@@ -231,8 +233,10 @@ class Plotter:
         )
         plt.close(fig)
 
+        plt.ion()
+
     def _make_plot(self, station: dict, time_scale: str):
-        plt.ioff() # Отключение интерактивного режима (чтобы окна при работе не мелькали)
+        plt.ioff()  # Отключение интерактивного режима (чтобы окна при работе не мелькали)
 
         footer_text = station["db_time_past"]["recent"].strftime("%d.%m.%Y %H:%M")
 
@@ -289,6 +293,7 @@ class Plotter:
             )
             out_filename = f"{station['code']}_{parameter['code']}_{self._config[f'{time_scale}-plot-suffix']}.png"
             out_filename = os.path.join(self._config["images-folder"], out_filename)
+
             fig = plt.gcf()
             plt.savefig(
                 out_filename,
@@ -297,6 +302,8 @@ class Plotter:
                 dpi=150,
             )
             plt.close(fig)
+
+        plt.ion()
 
     def make_plots(self, station: dict, time_depth: str):
         time_limit = {
@@ -335,3 +342,156 @@ class Plotter:
         if time_limit[time_depth]["value"] == 1:  # 1
             logging.info(f"Строю 1-дневные графики для станции {station['code']}")
             self._make_plot(station, "1d")
+
+    def make_plots_forecast(self, station: dict, time_depth: str, time_forecast: str):
+        logging.info(
+            "Строю графики наблюдений для станции %s за прошлые %s дней",
+            station["code"],
+            time_depth[:-1],
+        )
+        suffix_floc = ""
+        suffix_fglob = ""
+        if self.local_forecast:
+            logging.info(
+                "Добавляю график локального (уточненного) прогноза за прошлые %s дней и на будущие %s дней",
+                time_depth[:-1],
+                time_forecast[:-1],
+            )
+            suffix_floc = "_floc"
+
+        if self.glob_forecast:
+            logging.info(
+                "Добавляю график глобального (сырого) прогнозаза прошлые %s дней и на будущие %s дней",
+                time_depth[:-1],
+                time_forecast[:-1],
+            )
+            suffix_fglob = "_fglob"
+
+        plt.ioff()  # Отключение интерактивного режима (чтобы окна при работе не мелькали)
+
+        footer_text = station["db_time_past"]["recent"].strftime("%d.%m.%Y %H:%M")
+
+        time_scale = int(time_depth[:-1]) + int(time_forecast[:-1])
+
+        # Определение значений для оси Х
+        x_obs = station["db_time_past"][time_depth]
+        x_forecast_past = (
+            station["om_time_past"][time_depth] if "om_time_past" in station else []
+        )
+        x_forecast_future = (
+            station["om_time_future"][time_depth] if "om_time_future" in station else []
+        )
+
+        # Формирование границ для оси Х
+        if int(time_forecast[:-1]) > 0:
+            x_limits = [
+                station["db_time_range_past"][time_depth][0],
+                station["om_time_range_future"][time_depth][-1],
+            ]
+        else:
+            x_limits = station["db_time_range_past"][time_depth]
+
+        for _, parameter in station["parameters"].items():
+            plt.figure(figsize=(6.4, 4.8 - 0.8))
+            plt.xlim(mdates.date2num(x_limits))
+            ax = plt.gca()
+
+            # Отрисовка наблюдений
+
+            y_obs = parameter["data"][time_depth]
+            ax.plot(x_obs, y_obs, color="blue", label="Наблюдения")
+
+            # Добавление локального (скорректированного) прогноза
+            if self.local_forecast:
+                if "om_data_local" in parameter:
+                    # Отрисовка исторического интервала прогноза
+                    if "past" in parameter["om_data_local"]:
+                        y_local_past = parameter["om_data_local"]["past"][time_depth]
+                        ax.plot(
+                            x_forecast_past,
+                            y_local_past,
+                            color="green",
+                            label="Локальный прогноз",
+                        )
+
+                    # Отрисовка прогноза вперед
+                    if "future" in parameter["om_data_local"]:
+                        y_local_future = parameter["om_data_local"]["future"][
+                            time_depth
+                        ]
+                        ax.plot(x_forecast_future, y_local_future, "--", color="green")
+
+            # Добавление глобального (сырого) прогноза
+            if self.glob_forecast:
+                if "om_data_glob" in parameter:
+                    # Отрисовка исторического интервала прогноза
+                    if "past" in parameter["om_data_glob"]:
+                        y_glob_past = parameter["om_data_glob"]["past"][time_depth]
+                        ax.plot(
+                            x_forecast_past,
+                            y_glob_past,
+                            color="darkred",
+                            label="Глобальный прогноз",
+                        )
+
+                    # Отрисовка прогноза вперед
+                    if "future" in parameter["om_data_glob"]:
+                        y_glob_future = parameter["om_data_glob"]["future"][time_depth]
+                        ax.plot(x_forecast_future, y_glob_future, "--", color="darkred")
+
+            ax.set_ylabel(parameter["full_name"], size=13)
+            ax.tick_params("both", labelsize=12)
+            plt.title(station["full_name"], size=14)
+            plt.legend()
+
+            if time_scale == 1:
+                ax.xaxis.set_major_locator(mdates.HourLocator(interval=4))
+                ax.xaxis.set_minor_locator(mdates.HourLocator())
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+            elif time_scale <= 3:
+                ax.xaxis.set_major_locator(mdates.HourLocator(interval=24))
+                ax.xaxis.set_minor_locator(mdates.HourLocator(interval=6))
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d %H:%M"))
+            elif time_scale <= 7:
+                ax.xaxis.set_major_locator(mdates.HourLocator(interval=24))
+                ax.xaxis.set_minor_locator(mdates.HourLocator(interval=6))
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
+            elif time_scale <= 14:
+                ax.xaxis.set_major_locator(mdates.HourLocator(interval=48))
+                ax.xaxis.set_minor_locator(mdates.HourLocator(interval=12))
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
+            elif time_scale <= 30:
+                ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))
+                ax.xaxis.set_minor_locator(mdates.DayLocator())
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
+            else:
+                ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
+                ax.xaxis.set_minor_locator(mdates.DayLocator())
+                ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
+
+            ax.grid(True)
+            ax.grid(which="minor", linestyle=":")
+
+            plt.figtext(
+                0.95,
+                0.025,
+                footer_text,
+                horizontalalignment="right",
+                size=10,
+                weight="light",
+            )
+
+            # Сохранение в файл .png
+            out_filename = f"{station['code']}_{parameter['code']}_{time_depth}-past_{time_forecast}-future{suffix_floc}{suffix_fglob}.png"
+            out_filename = os.path.join(self._config["images-folder"], out_filename)
+
+            fig = plt.gcf()
+            plt.savefig(
+                out_filename,
+                edgecolor=fig.get_edgecolor(),
+                facecolor=fig.get_facecolor(),
+                dpi=150,
+            )
+            plt.close(fig)
+
+        # plt.ion()
