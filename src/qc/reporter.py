20dcc725e98e69231bd976b3846c88022c0a4f2b
Yuliya Martynova <foxyj13@gmail.com>
2026-08-12 18:17:22 +0700

    Заполнение init

--- src/qc/reporter.py
+++ src/qc/reporter.py
@@ -1,9 +1,23 @@
+import datetime
 import logging
 import pickle
 
 
 class Reporter:
-    def __init__(self, args): ...
+    def __init__(self, args: dict, now_dates: list[datetime.date]):
+        self.mode = args["main-args"]["mode"]
+        self.case = args["main-args"]["case"]
+
+        self.forecast_args = args["forecast-args"]
+
+        self.data_folder = args["data-args"]["data-folder"]
+        self.exp_name = args["data-args"]["experiment-name"]
+
+        self.output_folder = args["main-args"]["output-folder"]
+
+        self.now_dates = now_dates
+
+        # Формирование списка pkl-файлов
 
     def get_file_list(self):
         logging.info("Получение списка необходимых файлов с прогнозами")
